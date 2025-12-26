949bd8a53c01d9f88f647c881e3c925fe9dc3c54
Chris <chrisoconnell21@gmail.com>
2025-12-26 00:53:14 -0800

    0.11 fix 2

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1766735362949,
+    __BUILD_TIMESTAMP_MS__: 1766739003333,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
